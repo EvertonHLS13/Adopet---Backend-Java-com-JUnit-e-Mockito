ba42d0b232bf738f6201df5bf910d5c089743cb7
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2025-12-11 23:48:21 -0300

    Add files via upload

---
+++ src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComLimiteDeAdocoes.java
@@ -0,0 +1,27 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidacaoTutorComLimiteDeAdocoes implements ValidacaoSolicitacaoAdocao {
+
+    @Autowired
+    private AdocaoRepository adocaoRepository;
+
+    @Override
+    public void validar(SolicitacaoAdocaoDto dto) {
+
+        long quantidadeAprovadas =
+                adocaoRepository.countByTutorIdAndStatus(dto.idTutor(), StatusAdocao.APROVADO);
+
+        if (quantidadeAprovadas >= 5) {
+            throw new ValidacaoException("Tutor chegou ao limite máximo de 5 adoções!");
+        }
+    }
+
+}
---
+++ src/test/java/br/com/alura/adopet/api/controller/PetControllerTest.java
@@ -0,0 +1,74 @@
+package br.com.alura.adopet.api.controller;
+
+import br.com.alura.adopet.api.service.PetService;
+import br.com.alura.adopet.api.service.TutorService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PetControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PetService service;
+
+    @Test
+    void deveriaDevolverCodigo200paraRequisicaoDelistarTodosPetsDisponiveis() throws Exception {
+        //Act
+        MockHttpServletResponse response = mockMvc.perform(
+                get("/pets")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //Assert
+        assertEquals(200, response.getStatus());
+    }
+
+
+    @SpringBootTest
+    @AutoConfigureMockMvc
+    static
+    class TutorControllerTest {
+
+        @MockBean
+        private TutorService service;
+
+        @Autowired
+        private MockMvc mockMvc;
+
+        @Test
+        void deveriaDevolverCodigo200ParaRequisicaoDeCadastrarTutor() throws Exception {
+            // Arrange
+            String json = """
+                    {
+                    "nome": "Rodrigo",
+                    "telefone": "(21)0000-9090",
+                    "email": "email@exemple.com.br"
+                    }
+                    """;
+            //Act
+            MockHttpServletResponse response = mockMvc.perform(
+                    post("/tutores")
+                            .content(json)
+                            .contentType(MediaType.APPLICATION_JSON)
+            ).andReturn().getResponse();
+
+            //Assert
+            assertEquals(200, response.getStatus());
+        }
+
+    }
+}
